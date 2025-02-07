0c2bfede5fd9ecb5bd2ff220ca7accf06c9ef82f
Norluis Galvez <norly021196@gmail.com>
2025-02-07 15:50:01 -0500

    styles system

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer className="text-sm text-center">
+      Challenge by{" "}
+      <a
+        className="text-blue-400 underline"
+        href="https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://www.norly96.com"
+        className="text-blue-400 underline"
+        target="_blank"
+      >
+        Norly96
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
